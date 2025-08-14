42b84b81b43b9d1d953230df610e74fa31c54768
Durga Prasad Pragada <129383194+durgaprasadpragada@users.noreply.github.com>
2025-08-14 22:15:52 +0530

    Add files via upload

---
+++ 100days_of_cpp/Day02_Variables/variables_example.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 21;
+    double height = 5.9;
+    char grade = 'A';
+    bool isStudent = true;
+
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Student: " << isStudent << endl;
+
+    return 0;
+}
