783bb27984d2a6300f142631f586aefbecd73fa4
Durga Prasad Pragada <129383194+durgaprasadpragada@users.noreply.github.com>
2025-08-15 20:35:23 +0530

    Update variables_example.cpp
    explained why we use "endl"

--- 100days_of_cpp/Day02_Variables/variables_example.cpp
+++ 100days_of_cpp/Day02_Variables/variables_example.cpp
@@ -11,6 +11,13 @@ int main() {
     cout << "Height: " << height << endl;
     cout << "Grade: " << grade << endl;
     cout << "Student: " << isStudent << endl;
+    /*endl is used to get immidiate output Inserts a newline and flushes the output buffer.
+    Flushing means it forces all pending output to appear on the screen immediately.
+    Slower if used too often because flushing takes extra time.
+    Useful when:
+    You want immediate output (e.g., before taking input).
+    Writing to a file and want to make sure the text is saved right away.*/
+    
 
     return 0;
 }
