d473ab993f3f8e28909406aef53a69aee4f10a40
Durga Prasad Pragada <129383194+durgaprasadpragada@users.noreply.github.com>
2025-08-16 22:53:41 +0530

    file added
    about operators in c++

--- 100days_of_cpp/Day03_Operators/operators.cpp
+++ 100days_of_cpp/Day03_Operators/operators.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    int a = 10, b = 3;
+
+    // Arithmetic
+    cout << "a + b = " << a + b << "\n";
+    cout << "a - b = " << a - b << "\n";
+    cout << "a * b = " << a * b << "\n";
+    cout << "a / b = " << a / b << "\n"; // integer division
+    cout << "a % b = " << a % b << "\n";
+
+    // Relational
+    cout << "a == b: " << (a == b) << "\n";
+    cout << "a != b: " << (a != b) << "\n";
+    cout << "a > b: " << (a > b) << "\n";
+    cout << "a < b: " << (a < b) << "\n";
+
+    // logical
+    cout << "(a > 5 && b < 5): " << (a > 5 && b < 5) << "\n";
+    cout << "(a > 5 || b < 5): " << (a > 5 && b < 5) << "\n";
+    cout << "!(a == b): " << !(a == b) << "\n";
+
+    // Assignment
+    int c = a;
+    c += b;
+    cout << "C after c += b: " << c << "\n";
+
+    return 0;
+}
