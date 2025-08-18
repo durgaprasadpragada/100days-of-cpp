b4934cc0d3802da2612073ccb5ea7fcf21bdea0e
Durga Prasad Pragada <129383194+durgaprasadpragada@users.noreply.github.com>
2025-08-18 21:44:22 +0530

    while looooop
    Used when we don’t know the number of repetitions beforehand

--- 100days_of_cpp/Day05_loops/while_loop.cpp
+++ 100days_of_cpp/Day05_loops/while_loop.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+using namespace std;
 
+// while loop : Used when we don’t know the number of repetitions beforehand.
+int main()
+{
+    int i = 1;
+    while (i <= 5)
+    {
+        cout << "Iteration: " << i << "\n";
+        i++;
+    }
+
+    return 0;
+}
