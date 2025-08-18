ebc8249603ade2f72dcec87aa9d352db016c52ab
Durga Prasad Pragada <129383194+durgaprasadpragada@users.noreply.github.com>
2025-08-18 20:49:02 +0530

    for loop
    Best when we know how many times to repeat

---
+++ 100days_of_cpp/Day05_loops/for_loop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // for loop : Best when we know how many times to repeat
+    for (int i = 1; i <= 5; i++)
+    {
+        cout << "Iteration: " << i << "\n";
+    }
+
+    return 0;
+}
