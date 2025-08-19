d2f47e19a3e2bedaa03e70f249aa1b53c01bd4bf
Durga Prasad Pragada <129383194+durgaprasadpragada@users.noreply.github.com>
2025-08-19 20:40:54 +0530

    about function in cpp

---
+++ 100days_of_cpp/Day06_Functions/function.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// Function declaration
+void greetings()
+{
+    cout << "Hello! welcome to CPP...";
+}
+
+int main()
+{
+    greetings();
+    return 0;
+}
