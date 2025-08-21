041d17e5c7bb231abe1544b9bbc0b9ca8c4a26dd
Durga Prasad Pragada <129383194+durgaprasadpragada@users.noreply.github.com>
2025-08-21 22:49:37 +0530

    pointers
    A pointer is a variable that stores the address of another variable.

--- 100days_of_cpp/Day08_pointers_references/pointers.cpp
+++ 100days_of_cpp/Day08_pointers_references/pointers.cpp
@@ -1,1 +1,21 @@
+/*
+What is a Pointer?
+A pointer is a variable that stores the address of another variable.
+Normally variables store values, but pointers store locations in memory (RAM).
+*/
 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 10;
+    int *ptr = &x; // pointer to int
+
+    cout << "value of x: " << x << "\n";
+    cout << "Address of x: " << &x << "\n";
+    cout << "pointer stores: " << ptr << "\n";
+    cout << "value at pointer: " << *ptr << "\n";
+
+    return 0;
+}
