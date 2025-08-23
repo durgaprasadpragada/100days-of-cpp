6096464b7db65339e2e7b01b65d031b68efc42f1
Durga Prasad Pragada <129383194+durgaprasadpragada@users.noreply.github.com>
2025-08-23 22:52:24 +0530

    classes and objects
    Classes → Blueprints (like a plan of a car 🚗)
    Objects → Real entities created from class (the actual car you drive 🚙).

--- 100days_of_cpp/Day10_OOP/class_object.cpp
+++ 100days_of_cpp/Day10_OOP/class_object.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+using namespace std;
 
+class Student
+{       // Class definition
+public: // Access specifier
+    string name;
+    int age;
+
+    void display() // Member function
+    {
+        cout << "Name: " << name << ", Age: " << age << "\n";
+    }
+};
+
+int main()
+{
+    Student s1; // Object created
+    s1.name = "Prasad";
+    s1.age = 21;
+
+    s1.display(); // Call function of class
+
+    return 0;
+}
+
+/*
+class Student → defines properties (data) & behaviors (functions).
+s1 → object of Student.
+s1.name = "prasad"; → accessing class data.
+s1.display() → calling method
+*/
