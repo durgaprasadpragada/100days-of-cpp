cacc1d902948b770961fbde4e192d7c483775abd
Durga Prasad Pragada <129383194+durgaprasadpragada@users.noreply.github.com>
2025-08-27 21:55:05 +0530

    Inline Functions
    Normally, when you call a function → control goes to the function’s code (function call overhead).
    Inline functions remove this overhead by replacing the function call with the actual function code during compilation.
    Declared with the inline keyword

--- 100days_of_cpp/Day14_Inline_keyword/Inline_function.cpp
+++ 100days_of_cpp/Day14_Inline_keyword/Inline_function.cpp
@@ -1,1 +1,21 @@
+/*
+What is an Inline Function?
+Normally, when you call a function → control goes to the function’s code (function call overhead).
+Inline functions remove this overhead by replacing the function call with the actual function code during compilation.
+Declared with the inline keyword
+*/
 
+#include <iostream>
+using namespace std;
+
+// Inline function definition
+inline int sqrt(int n)
+{
+    return n * n;
+}
+int main()
+{
+    cout << "Square root of 5 is : " << sqrt(5) << endl;
+    cout << "Square root of 10 is : " << sqrt(10) << endl;
+    return 0;
+}
