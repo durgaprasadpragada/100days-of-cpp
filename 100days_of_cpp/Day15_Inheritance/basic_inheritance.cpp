e414930ac7d0a3c922630d57530420e5080b874e
Durga Prasad Pragada <129383194+durgaprasadpragada@users.noreply.github.com>
2025-08-28 22:28:16 +0530

    Inheritance
    What is Inheritance?
    Inheritance allows a class (child/derived class) to acquire properties & behaviors of another class (parent/base class).
    Promotes reusability and hierarchical relationships.

--- 100days_of_cpp/Day15_Inheritance/basic_inheritance.cpp
+++ 100days_of_cpp/Day15_Inheritance/basic_inheritance.cpp
@@ -1,1 +1,33 @@
+// Basic Inheritance
 
+#include <iostream>
+using namespace std;
+
+// Base class
+class Vehicle
+{
+public:
+    void start()
+    {
+        cout << "Vehicle started" << endl;
+    }
+};
+
+// Derived class
+class Car : public Vehicle
+{
+public:
+    void drive()
+    {
+        cout << "Car is driving" << endl;
+    }
+};
+
+int main()
+{
+    Car myCar;
+    myCar.start(); // Inherited from Vehicle
+    myCar.drive(); // Car’s own function
+
+    return 0;
+}
