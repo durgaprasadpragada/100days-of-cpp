9a4245ce46c457a18a4b0e4c2a50fd94c5702ad5
Durga Prasad Pragada <129383194+durgaprasadpragada@users.noreply.github.com>
2025-08-28 22:28:48 +0530

    Add files via upload
    What is Inheritance?
    Inheritance allows a class (child/derived class) to acquire properties & behaviors of another class (parent/base class).
    Promotes reusability and hierarchical relationships.

--- 100days_of_cpp/Day15_Inheritance/single_inheritance_private_data.cpp
+++ 100days_of_cpp/Day15_Inheritance/single_inheritance_private_data.cpp
@@ -1,1 +1,46 @@
+// Single Inheritance (with private data)
 
+#include <iostream>
+using namespace std;
+
+class Person
+{
+protected:
+    string name;
+    int age;
+
+public:
+    void setDetails(string n, int a)
+    {
+        name = n;
+        age = a;
+    }
+};
+
+class Student : public Person
+{
+private:
+    int rollNo;
+
+public:
+    void setRoll(int r)
+    {
+        rollNo = r;
+    }
+
+    void display()
+    {
+        cout << "Name: " << name << ", Age: " << age
+             << ", Roll No: " << rollNo << endl;
+    }
+};
+
+int main()
+{
+    Student s1;
+    s1.setDetails("Alice", 20);
+    s1.setRoll(101);
+    s1.display();
+
+    return 0;
+}
