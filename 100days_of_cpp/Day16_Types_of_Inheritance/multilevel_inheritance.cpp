d57d219c18c4c85c47927f0bdd1384e219c9dea1
Durga Prasad Pragada <129383194+durgaprasadpragada@users.noreply.github.com>
2025-08-29 22:30:46 +0530

    Add files via upload

--- 100days_of_cpp/Day16_Types_of_Inheritance/multilevel_inheritance.cpp
+++ 100days_of_cpp/Day16_Types_of_Inheritance/multilevel_inheritance.cpp
@@ -1,1 +1,42 @@
+/*
+Multilevel Inheritance:
+A derived class becomes the base class for another class.
+*/
+#include <iostream>
+using namespace std;
 
+class Person
+{
+public:
+    void showPerson()
+    {
+        cout << "I am a person" << endl;
+    }
+};
+
+class Student : public Person
+{
+public:
+    void showStudent()
+    {
+        cout << "I am a student" << endl;
+    }
+};
+
+class Graduate : public Student
+{
+public:
+    void showGraduate()
+    {
+        cout << "I am a Graduate student" << endl;
+    }
+};
+
+int main()
+{
+    Graduate g;
+    g.showPerson();
+    g.showStudent();
+    g.showGraduate();
+    return 0;
+}
