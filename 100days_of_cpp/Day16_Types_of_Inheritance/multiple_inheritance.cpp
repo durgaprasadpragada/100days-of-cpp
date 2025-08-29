544c7de3618a17d9fe4657a0f465e0aed7c5b2fd
Durga Prasad Pragada <129383194+durgaprasadpragada@users.noreply.github.com>
2025-08-29 22:30:24 +0530

    Add files via upload

--- 100days_of_cpp/Day16_Types_of_Inheritance/multiple_inheritance.cpp
+++ 100days_of_cpp/Day16_Types_of_Inheritance/multiple_inheritance.cpp
@@ -1,1 +1,41 @@
+/*
+Multiple Inheritance:
+One derived class inherits from more than one base class
+*/
+#include <iostream>
+using namespace std;
 
+class Engine
+{
+public:
+    void engineType()
+    {
+        cout << "V8 Engine.." << endl;
+    }
+};
+class Wheels
+{
+public:
+    void wheelType()
+    {
+        cout << "Alloy Wheels" << endl;
+    }
+};
+
+class Car : public Engine, public Wheels
+{
+public:
+    void showCar()
+    {
+        cout << "This is a sports car..." << endl;
+    }
+};
+
+int main()
+{
+    Car c;
+    c.engineType();
+    c.wheelType();
+    c.showCar();
+    return 0;
+}
