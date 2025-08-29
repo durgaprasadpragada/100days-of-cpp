c7bfb7462fb826b082bb6dedb59a1897ecee1ccd
Durga Prasad Pragada <129383194+durgaprasadpragada@users.noreply.github.com>
2025-08-29 22:29:41 +0530

    Add files via upload

--- 100days_of_cpp/Day16_Types_of_Inheritance/single_inheritance.cpp
+++ 100days_of_cpp/Day16_Types_of_Inheritance/single_inheritance.cpp
@@ -1,1 +1,33 @@
+/*
+Single Inheritance:
+One base class, one derived class.
+Most common form of inheritance.
+*/
+#include <iostream>
+using namespace std;
 
+class Animal
+{
+public:
+    void eat()
+    {
+        cout << "Eating..." << endl;
+    }
+};
+
+class Dog : public Animal
+{
+public:
+    void bark()
+    {
+        cout << "Barking..." << endl;
+    }
+};
+
+int main()
+{
+    Dog d;
+    d.eat();  // Inherited
+    d.bark(); // Own function
+    return 0;
+}
