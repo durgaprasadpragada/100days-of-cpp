a1e9dddad22a9a6f0a4881ca25d027c054305e2f
Durga Prasad Pragada <129383194+durgaprasadpragada@users.noreply.github.com>
2025-08-30 22:34:58 +0530

    Add files via upload

--- 100days_of_cpp/Day17_Hybrid & Hierarchical Inheritance/hybrid_inheritance.cpp
+++ 100days_of_cpp/Day17_Hybrid & Hierarchical Inheritance/hybrid_inheritance.cpp
@@ -1,1 +1,47 @@
+#include <iostream>
+using namespace std;
 
+class Person
+{
+public:
+    void showPerson()
+    {
+        cout << "I am a Person" << endl;
+    }
+};
+class Student : public Person
+{
+public:
+    void showStudent()
+    {
+        cout << "I am a student" << endl;
+    }
+};
+
+class Teacher : public Person
+{
+public:
+    void showTeacher()
+    {
+        cout << "I am a Teacher" << endl;
+    }
+};
+
+class TeachingAssistant : public Student, public Teacher
+{
+public:
+    void showTA()
+    {
+        cout << "I am a Teaching Assistant" << endl;
+    }
+};
+
+int main()
+{
+    TeachingAssistant ta;
+    ta.showStudent();
+    ta.showTeacher();
+    ta.showTA();
+    // ta.showPerson(); // ❌ Ambiguity if called directly (diamond problem)
+    return 0;
+}
