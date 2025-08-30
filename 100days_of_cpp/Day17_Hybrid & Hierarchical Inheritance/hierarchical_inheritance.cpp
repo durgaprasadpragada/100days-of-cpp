75943a266e573a2faf1d2911b31f552baa19417e
Durga Prasad Pragada <129383194+durgaprasadpragada@users.noreply.github.com>
2025-08-30 22:35:19 +0530

    Add files via upload

--- 100days_of_cpp/Day17_Hybrid & Hierarchical Inheritance/hierarchical_inheritance.cpp
+++ 100days_of_cpp/Day17_Hybrid & Hierarchical Inheritance/hierarchical_inheritance.cpp
@@ -1,1 +1,47 @@
+/*
+Hierarchical Inheritance:
+One base class → Multiple derived classes.
+Like a family tree
+*/
 
+#include <iostream>
+using namespace std;
+
+class Animal
+{
+public:
+    void eat()
+    {
+        cout << "Eating..." << endl;
+    }
+};
+class Dog : public Animal
+{
+public:
+    void Bark()
+    {
+        cout << "Barking..." << endl;
+    }
+};
+class Cat : public Animal
+{
+public:
+    void Meow()
+    {
+        cout << "Meowing..." << endl;
+    }
+};
+
+int main()
+{
+    Dog d;
+    Cat c;
+
+    d.eat();  // From Animal
+    d.Bark(); // Dog-specific
+
+    c.eat();  // From Animal
+    c.Meow(); // Cat-specific
+
+    return 0;
+}
