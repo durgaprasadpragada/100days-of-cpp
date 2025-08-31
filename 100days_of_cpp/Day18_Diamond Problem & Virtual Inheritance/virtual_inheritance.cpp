ea6d44105a9dd1dfece0124cf6f177143472630f
Durga Prasad Pragada <129383194+durgaprasadpragada@users.noreply.github.com>
2025-08-31 19:19:41 +0530

    Add files via upload
    We use the virtual keyword while inheriting A in B & C.
    This ensures only one copy of A is shared.

--- 100days_of_cpp/Day18_Diamond Problem & Virtual Inheritance/virtual_inheritance.cpp
+++ 100days_of_cpp/Day18_Diamond Problem & Virtual Inheritance/virtual_inheritance.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
 
+class A
+{
+public:
+    void display()
+    {
+        cout << "Class A function" << endl;
+    }
+};
+
+class B : virtual public A{};
+class C : virtual public A{};
+class D : public B, public C{};
+
+int main()
+{
+    D obj;
+    obj.display(); // ✅ No ambiguity
+    return 0;
+}
