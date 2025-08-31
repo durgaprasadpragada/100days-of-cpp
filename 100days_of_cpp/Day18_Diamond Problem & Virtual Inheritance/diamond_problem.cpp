14e3162151db69ab63f9a0439107da1a3b483d62
Durga Prasad Pragada <129383194+durgaprasadpragada@users.noreply.github.com>
2025-08-31 19:20:32 +0530

    Add files via upload
    .       A
           / \
          B   C
           \ /
            D
    Class A → Base class
    
    B & C → Derived from A
    
    D → Derived from both B & C
    
    👉 Problem: D gets two copies of A (from B and C).
    This creates ambiguity when calling functions from A.

--- 100days_of_cpp/Day18_Diamond Problem & Virtual Inheritance/diamond_problem.cpp
+++ 100days_of_cpp/Day18_Diamond Problem & Virtual Inheritance/diamond_problem.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
 
+class A
+{
+public:
+    void display()
+    {
+        cout << "Class A function" << endl;
+    }
+};
+class B : public A{};
+class C : public A{};
+class D : public B, public C{};
+
+int main()
+{
+    D obj;
+    // obj.display();  // ❌ Error: Ambiguous, two copies of A
+    obj.B::display(); // ✅ Specify path
+    obj.C::display(); // ✅ Specify path
+    return 0;
+}
