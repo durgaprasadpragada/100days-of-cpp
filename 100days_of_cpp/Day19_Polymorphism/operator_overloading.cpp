303e63f4792a755b5dc9cc741a08199f6266e3a4
Durga Prasad Pragada <129383194+durgaprasadpragada@users.noreply.github.com>
2025-09-01 22:39:05 +0530

    Operator Overloading
    Operator overloading means giving extra meaning to operators (+, -, *, ==, etc.) when they are used with user-defined objects (classes).
    For example:
    Normally + is used for integers (2 + 3 = 5).
    But what if we have two complex numbers (like 2 + 3i and 1 + 7i)?
    We can’t just do c1 + c2 directly.
    So, we overload + to work with Complex objects.
    So basically, operator overloading is just a fancy function call, but lets you use operators naturally with objects.

--- 100days_of_cpp/Day19_Polymorphism/operator_overloading.cpp
+++ 100days_of_cpp/Day19_Polymorphism/operator_overloading.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+using namespace std;
 
+class Complex
+{
+    int real, imag;
+
+public:
+    Complex(int r = 0, int i = 0) // constructor
+    {
+        real = r;
+        imag = i;
+    }
+    // Operator overloading function for +
+    Complex operator+(Complex const &obj)
+    {
+        Complex result;
+        result.real = real + obj.real;
+        result.imag = imag + obj.imag;
+        return result;
+    }
+    void display()
+    {
+        cout << real << " + " << imag << "i" << endl;
+    }
+};
+
+int main()
+{
+    Complex c1(2, 3), c2(1, 7);
+    Complex c3 = c1 + c2;
+    c3.display();
+    return 0;
+}
