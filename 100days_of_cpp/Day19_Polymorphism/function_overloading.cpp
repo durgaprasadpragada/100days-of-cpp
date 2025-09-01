b58ef1cbc1741979e64e0a81296ca9a0636591fb
Durga Prasad Pragada <129383194+durgaprasadpragada@users.noreply.github.com>
2025-09-01 22:37:36 +0530

    Add files via upload
    Function Overloading

--- 100days_of_cpp/Day19_Polymorphism/function_overloading.cpp
+++ 100days_of_cpp/Day19_Polymorphism/function_overloading.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+using namespace std;
 
+class Math
+{
+public:
+    int add(int a, int b)
+    {
+        return a + b;
+    }
+    double add(double a, double b)
+    {
+        return a + b;
+    }
+    int add(int a, int b, int c)
+    {
+        return a + b + c;
+    }
+};
+int main()
+{
+    Math obj;
+    cout << "Sum (int):" << obj.add(2, 5) << endl;
+    cout << "Sum (double):" << obj.add(2.5, 3.5) << endl;
+    cout << "Sum (3 ints):" << obj.add(4, 5, 6) << endl;
+    return 0;
+}
