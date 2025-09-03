5ba6d4d6d4039789950707869725dc3525cc8d5b
Durga Prasad Pragada <129383194+durgaprasadpragada@users.noreply.github.com>
2025-09-03 22:09:40 +0530

    Add files via upload
    Abstract classes are blueprints for other classes.
    
    They allow us to enforce a contract: “Every derived class must implement this function.”
    
    If a derived class does not implement all pure virtual functions → it also becomes abstract.

--- 100days_of_cpp/Day21_Abstract classes & Pure Virtual Functions/basic_abstract_class.cpp
+++ 100days_of_cpp/Day21_Abstract classes & Pure Virtual Functions/basic_abstract_class.cpp
@@ -1,1 +1,49 @@
+/*
+An abstract class is a class that cannot be instantiated. It is meant to be used only as a base class.
+A class becomes abstract when it has at least one pure virtual function.
+*/
 
+#include <iostream>
+using namespace std;
+
+// Abstract class
+class Shape
+{
+public:
+    // pure virtual function
+    virtual void draw() = 0;
+};
+
+class Circle : public Shape
+{
+public:
+    void draw() override
+    {
+        cout << "Drawing a Circle" << endl;
+    }
+};
+
+class Rectangle : public Shape
+{
+public:
+    void draw() override
+    {
+        cout << "Drawing a Rectangle" << endl;
+    }
+};
+
+int main()
+{
+    Shape *s; // Base class pointer
+    Circle c;
+    Rectangle r;
+
+    s = &c;
+    s->draw(); // Calls Circle's draw()
+
+    s = &r; // Calls Rectangle's draw()
+    s->draw();
+
+    // Shape s1; [[Error: u cannot instantiate abstract class]]
+    return 0;
+}
