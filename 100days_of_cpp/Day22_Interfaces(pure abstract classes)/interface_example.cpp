4e741c316b4e76f5292235b6aa17214623d7748a
Durga Prasad Pragada <129383194+durgaprasadpragada@users.noreply.github.com>
2025-09-04 22:39:43 +0530

    A class can implement multiple interfaces
    Rules of Interfaces:
    
    All member functions are pure virtual (=0).
    No data members allowed (only constants, if needed).
    s (multiple inheritance).
    Used to enforce strict contracts → derived classes must implement all functions.

--- 100days_of_cpp/Day22_Interfaces(pure abstract classes)/interface_example.cpp
+++ 100days_of_cpp/Day22_Interfaces(pure abstract classes)/interface_example.cpp
@@ -1,1 +1,56 @@
+#include <iostream>
+using namespace std;
 
+// Interface (pure abstract class)
+class IShape
+{
+public:
+    virtual void draw() = 0;
+    virtual double area() = 0;
+};
+class Circle : public IShape
+{
+    double radius;
+
+public:
+    Circle(double r) : radius(r) {}
+    void draw() override
+    {
+        cout << "Drawing Circle" << endl;
+    }
+    double area() override
+    {
+        return 3.14 * radius * radius;
+    }
+};
+class Square : public IShape
+{
+    double side;
+
+public:
+    Square(double s) : side(s) {}
+    void draw() override
+    {
+        cout << "Drawing Square" << endl;
+    }
+    double area() override
+    {
+        return side * side;
+    }
+};
+int main()
+{
+    IShape *shape;
+    Circle c(5);
+    Square s(4);
+
+    shape = &c;
+    shape->draw();
+    cout << "Area: " << shape->area() << endl;
+
+    shape = &s;
+    shape->draw();
+    cout << "Area: " << shape->area() << endl;
+
+    return 0;
+}
--- 100days_of_cpp/Day22_Interfaces(pure abstract classes)/multiple_interface.cpp
+++ 100days_of_cpp/Day22_Interfaces(pure abstract classes)/multiple_interface.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+using namespace std;
 
+// Interface 1
+class IReadable
+{
+public:
+    virtual void read() = 0;
+};
+// Interface 2
+class IWritable
+{
+public:
+    virtual void write() = 0;
+};
+// Class implementing both interfaces
+class File : public IReadable, public IWritable
+{
+public:
+    void read() override
+    {
+        cout << "Reading from file..." << endl;
+    }
+    void write() override
+    {
+        cout << "Writing to file..." << endl;
+    }
+};
+int main()
+{
+    File f;
+    f.read();
+    f.write();
+
+    return 0;
+}
