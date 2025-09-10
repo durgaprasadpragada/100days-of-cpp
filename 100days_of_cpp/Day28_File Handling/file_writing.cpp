d85885a4fe483f28be6372d17885b695c30794c1
Durga Prasad Pragada <129383194+durgaprasadpragada@users.noreply.github.com>
2025-09-10 21:55:11 +0530

    writing a file
    In C++, file handling is done using the fstream library.
    
    ofstream → write to file (output stream)
    
    ifstream → read from file (input stream)
    
    fstream → both read & write

--- 100days_of_cpp/Day28_File Handling/file_writing.cpp
+++ 100days_of_cpp/Day28_File Handling/file_writing.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
 
+/*
+ofstream → write to file (output stream)
+ifstream → read from file (input stream)
+fstream → both read & write
+*/
+int main()
+{
+    ofstream file("example.txt"); // open file for writing
+    if (!file)
+    {
+        cout << "Error opening file!" << endl;
+        return 1;
+    }
+    file << "Hello, C++ File Handling!" << endl;
+    file << "This text is saved permanently." << endl;
+    file.close();
+    cout << "Data written to file successfully!" << endl;
+    return 0;
+}
