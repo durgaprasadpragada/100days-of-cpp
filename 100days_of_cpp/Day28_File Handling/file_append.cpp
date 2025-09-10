22cdb893bd41f418ddd949dd5b336c22c48300a0
Durga Prasad Pragada <129383194+durgaprasadpragada@users.noreply.github.com>
2025-09-10 21:58:57 +0530

    open file in append mode
    ios::app is used ,,,, so the file won’t be overwritten, it will just add more text.

--- 100days_of_cpp/Day28_File Handling/file_append.cpp
+++ 100days_of_cpp/Day28_File Handling/file_append.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
 
+int main()
+{
+    ofstream file("example.txt", ios::app); // open in append mode
+    // Now the file won’t be overwritten, it will just add more text.
+    file << "Adding a new line at the end." << endl;
+    file.close();
+    cout << "New line appended to file!" << endl;
+    return 0;
+}
+
+// now run file_reading.cpp to see appended line...
