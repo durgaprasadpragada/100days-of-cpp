f4133744690ff9c5403689c6ec4ae2ddb57d2dfc
Durga Prasad Pragada <129383194+durgaprasadpragada@users.noreply.github.com>
2025-09-10 21:57:01 +0530

    reading content form a file
    In C++, file handling is done using the fstream library.
    
    ofstream → write to file (output stream)
    
    ifstream → read from file (input stream)
    
    fstream → both read & write

--- 100days_of_cpp/Day28_File Handling/file_reading.cpp
+++ 100days_of_cpp/Day28_File Handling/file_reading.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 
+int main()
+{
+    ifstream file("example.txt"); // open file for reading
+    if (!file)
+    {
+        cout << "File not found!" << endl;
+        return 1;
+    }
+    string line;
+    while (getline(file, line))
+    {
+        cout << line << endl; // This reads the contents of example.txt and displays it.
+    }
+    file.close();
+    return 0;
+}
