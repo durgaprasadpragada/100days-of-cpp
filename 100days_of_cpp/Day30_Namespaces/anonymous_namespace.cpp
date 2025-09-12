0f5dfd859183da0419642f795210bb2390b5e1a2
Durga Prasad Pragada <129383194+durgaprasadpragada@users.noreply.github.com>
2025-09-12 23:08:51 +0530

    Update anonymous_namespace.cpp
    If you don’t give a name, the namespace is local to the file (like static scope in C).

--- 100days_of_cpp/Day30_Namespaces/anonymous_namespace.cpp
+++ 100days_of_cpp/Day30_Namespaces/anonymous_namespace.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+using namespace std;
+
+namespace {
+    void hidden() {
+        cout << "I am inside an anonymous namespace" << endl;
+    }
+}
+
+int main() {
+    hidden();
+    return 0;
+}
 
