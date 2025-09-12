527a41d4033604382e05db46d829d979a4b98862
Durga Prasad Pragada <129383194+durgaprasadpragada@users.noreply.github.com>
2025-09-12 23:02:27 +0530

    Update namespace.cpp
    A namespace is like a container for identifiers (functions, variables, classes).
    
    Prevents name collisions in big programs.

--- 100days_of_cpp/Day30_Namespaces/namespace.cpp
+++ 100days_of_cpp/Day30_Namespaces/namespace.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
+
+// Defining a namespace
+namespace MyNamespace {
+    int value = 42;
+
+    void display() {
+        cout << "Value inside MyNamespace: " << value << endl;
+    }
+}
+
+int main() {
+    // Accessing using namespace name
+    cout << MyNamespace::value << endl;
+    MyNamespace::display();
+
+    return 0;
+}
 
