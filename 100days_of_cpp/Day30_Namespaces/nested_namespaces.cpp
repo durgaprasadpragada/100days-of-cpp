4e2556b40a16ed3f3a072fd70e2180160c10515d
Durga Prasad Pragada <129383194+durgaprasadpragada@users.noreply.github.com>
2025-09-12 23:08:11 +0530

    Update nested_namespaces.cpp
    You can create namespaces inside other namespaces

--- 100days_of_cpp/Day30_Namespaces/nested_namespaces.cpp
+++ 100days_of_cpp/Day30_Namespaces/nested_namespaces.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+namespace Outer {
+    namespace Inner {
+        void message() {
+            std::cout << "Hello from Inner namespace!" << std::endl;
+        }
+    }
+}
+
+int main() {
+    Outer::Inner::message();
+    return 0;
+}
 
