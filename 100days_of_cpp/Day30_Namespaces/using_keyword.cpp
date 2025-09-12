c691fd3e88553de57058ce9bcf9df88d151cdea2
Durga Prasad Pragada <129383194+durgaprasadpragada@users.noreply.github.com>
2025-09-12 23:07:38 +0530

    Update using_keyword.cpp
    Instead of writing MyNamespace:: every time, you can use using:

--- 100days_of_cpp/Day30_Namespaces/using_keyword.cpp
+++ 100days_of_cpp/Day30_Namespaces/using_keyword.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+using namespace std;
+
+namespace First {
+    void hello() {
+        cout << "Hello from First namespace" << endl;
+    }
+}
+
+namespace Second {
+    void hello() {
+        cout << "Hello from Second namespace" << endl;
+    }
+}
+
+int main() {
+    First::hello();   // Access with scope resolution
+    Second::hello();
+
+    using namespace First;  // Now we can use directly
+    hello();
+
+    return 0;
+}
 
