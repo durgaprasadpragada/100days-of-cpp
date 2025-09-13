7bad1215912a514414b131cbbb2a7b8cf20ee155
Durga Prasad Pragada <129383194+durgaprasadpragada@users.noreply.github.com>
2025-09-13 22:09:39 +0530

    Update stack_unwinding.cpp
    destructors are called automatically when exception propagates.

--- 100days_of_cpp/Day31_Stack Unwinding & Re-throwing/stack_unwinding.cpp
+++ 100days_of_cpp/Day31_Stack Unwinding & Re-throwing/stack_unwinding.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+using namespace std;
 
+class Demo {
+public:
+    Demo() { cout << "Constructor called!" << endl; }
+    ~Demo() { cout << "Destructor called!" << endl; }
+};
+
+void func2() {
+    Demo d2;
+    cout << "Inside func2()" << endl;
+    throw runtime_error("Exception in func2");
+}
+
+void func1() {
+    Demo d1;
+    cout << "Inside func1()" << endl;
+    func2();
+}
+
+int main() {
+    try {
+        func1();
+    } catch (const exception& e) {
+        cout << "Caught Exception: " << e.what() << endl;
+    }
+    return 0;
+}
+
+/*
+output:
+Constructor called!
+Inside func1()
+Constructor called!
+Inside func2()
+Destructor called!   ← d2 destroyed (stack unwinding)
+Destructor called!   ← d1 destroyed
+Caught Exception: Exception in func2
+*/
