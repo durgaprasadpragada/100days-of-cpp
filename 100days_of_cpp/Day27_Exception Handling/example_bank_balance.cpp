aca0a8e1045c4c56309fa4f92ee4050b5eaab8e9
Durga Prasad Pragada <129383194+durgaprasadpragada@users.noreply.github.com>
2025-09-14 21:52:46 +0530

    Update example_bank_balance.cpp
    Create custom exception classes by inheriting std::exception.
    
    Override the what() method to give custom messages.
    
    Throw your exception with throw MyException();.
    
    Catch with catch(const exception& e) or specific type.

--- 100days_of_cpp/Day27_Exception Handling/example_bank_balance.cpp
+++ 100days_of_cpp/Day27_Exception Handling/example_bank_balance.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+#include <exception>
+using namespace std;
 
+class InsufficientBalance : public exception
+{
+public:
+    const char *what() const noexcept override
+    {
+        return "Error: Insufficient balance for withdrawl!";
+    }
+};
+void withdraw(double balance, double amount)
+{
+    if (amount > balance)
+    {
+        throw InsufficientBalance();
+    }
+    cout << "Withdrawl successfull Remaining balance: " << balance - amount << endl;
+}
+int main()
+{
+    try
+    {
+        {
+            withdraw(1000, 500);  // OK
+            withdraw(1000, 1500); // throws exception
+        }
+    }
+    catch (const exception &e)
+    {
+        cout << "Caught Exception: " << e.what() << endl;
+    }
+    return 0;
+}
+/*
+Output:
+Withdrawal successful! Remaining balance: 500
+Caught Exception: Error: Insufficient balance for withdrawal!
+*/
