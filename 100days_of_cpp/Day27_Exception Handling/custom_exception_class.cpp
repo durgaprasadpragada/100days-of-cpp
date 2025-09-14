acc354fdd700855db38435c165155901fa908929
Durga Prasad Pragada <129383194+durgaprasadpragada@users.noreply.github.com>
2025-09-14 21:35:26 +0530

    Update custom_exception_class.cpp
    output: Caught Exception: Error: Division by zero not allowed!

--- 100days_of_cpp/Day27_Exception Handling/custom_exception_class.cpp
+++ 100days_of_cpp/Day27_Exception Handling/custom_exception_class.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+#include <exception>
+using namespace std;
+
+class DivideByZero : public exception {
+public:
+    const char* what() const noexcept override {
+        return "Error: Division by zero not allowed!";
+    }
+};
+
+double divide(double a, double b) {
+    if (b == 0) {
+        throw DivideByZero();  // throw custom exception
+    }
+    return a / b;
+}
+
+int main() {
+    try {
+        cout << divide(10, 2) << endl;
+        cout << divide(5, 0) << endl;  // throws exception
+    }
+    catch (const exception& e) {
+        cout << "Caught Exception: " << e.what() << endl;
+    }
+    return 0;
+}
 
