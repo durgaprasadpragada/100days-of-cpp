02b228df8c534a8843b6d669df143c3c804a818f
Durga Prasad Pragada <129383194+durgaprasadpragada@users.noreply.github.com>
2025-09-16 22:59:26 +0530

    A vector is a dynamic array
    A vector is a dynamic array (it grows and shrinks in size).
    Header: #include <vector>

--- 100days_of_cpp/Day33_Standard_Template_Library/basic_vector.cpp
+++ 100days_of_cpp/Day33_Standard_Template_Library/basic_vector.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+int main()
+{
+    vector<int> v; // empty vector
+
+    // Insert elements
+    v.push_back(10);
+    v.push_back(20);
+    v.push_back(30);
+
+    // Access elements
+    cout << "First element: " << v[0] << endl;
+    cout << "Second Element: " << v.at(1) << endl;
+
+    // Size & Capacity
+    cout << "Size: " << v.size() << endl;
+    cout << "Capacity: " << v.capacity() << endl;
+
+    return 0;
+}
